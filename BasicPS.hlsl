9fa2941fb5e84367a4c0127e37b789ac1dd9b64f
DaikiWakui <k021g1119@g.neec.ac.jp>
2022-05-11 13:19:24 +0900

    03_02_シェーダに色を渡す

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -1,6 +1,6 @@
-cbffer ConstBufferdatamaterial : register(b0) {
-	float4 color;
-};
+cbuffer ConstBufferDataMaterial : register(b0) {
+	float4 color;	//êF(RGBA)
+}
 
 float4 main(float4 pos : SV_POSITION) : SV_TARGET
 {
